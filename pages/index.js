4d4a332586bd4516afe4554a2c2072a3ff6a565c
Weeravit Sirita <weeravit.sirita@gmail.com>
2017-12-16 12:03:25 +0700

    added elements to index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
-import MainLayout from '../src/components/common/MainLayout'
+import MainLayout from '../src/component/common/MainLayout'
 
 export default () => (
     <MainLayout>
-        <div>Welcome to next.js!</div>
+        <section class="hero is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        Welcome to Buffet Restaurant
+                    </h1>
+
+                    <br/>
+
+                    <p class="subtitle">
+                        How many people will come ?
+                    </p>
+
+                    <div class="field is-grouped is-grouped-centered">
+                        <div class="control">
+                            <div class="select is-medium">
+                                <select>
+                                    <option>Select dropdown</option>
+                                    <option>With options</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br/>
+
+                    <a class="button is-success is-medium">Confirm</a>
+                </div>
+            </div>
+        </section>
     </MainLayout>
 );
--- src/component/common/MainLayout.js
+++ src/component/common/MainLayout.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default (props) => (
     <div>
         <Head>
-            <meta charset="utf-8"/>
+            <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
             <title>Restaurant Bill Calculator</title>
